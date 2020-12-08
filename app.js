e13b79d00de3b46c9f7d159c30535aaa1b0c7bc3
rivanshubansal <75182177+rivanshubansal@users.noreply.github.com>
2020-12-08 22:20:59 +0530

    all basic done

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURl(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server: try after some time")
+}
 
 function clickHandler() {
-    outputDiv.innerText = "Rivanshu " + txtInput.value;
+  // outputDiv.innerText = "Rivanshu " + txtInput.value;
+  var inputText = txtInput.value; // taking input
+  // calling server for processing
+  fetch(getTranslationURl(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; // output
+    })
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
+
+
+
+// https://repl.it/@tanaypratap/lessonfourapi
